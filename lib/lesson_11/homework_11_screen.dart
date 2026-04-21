b912034d68bc6f414fd54478b068159c1ae832aa
Alisa <alisadmsiis@gmail.com>
2026-04-21 16:33:39 +0300

    feat(FLUTTER-11_3): added 3 part of homework to lesson 11

--- lib/lesson_11/homework_11_screen.dart
+++ lib/lesson_11/homework_11_screen.dart
@@ -10,35 +10,73 @@ class WidgetsLessonElevenScreen extends StatelessWidget {
 
     return Scaffold(
       body: Center(
-        child: Container(
-          width: cardWidth,
-          height: cardHeight,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(Icons.star, color: Colors.yellow),
-                  SizedBox(width: 10),
-                  Text(
-                    'Привіт, Flutter!',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Icon(Icons.star, color: Colors.yellow),
-                ],
-              ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 8,
+          children: const [
+            ColorfullContainerWidget(
+              cardWidth: cardWidth,
+              cardHeight: cardHeight,
+              color: Colors.blue,
             ),
+            ColorfullContainerWidget(
+              cardWidth: cardWidth,
+              cardHeight: cardHeight,
+              color: Colors.green,
+            ),
+            ColorfullContainerWidget(
+              cardWidth: cardWidth,
+              cardHeight: cardHeight,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ColorfullContainerWidget extends StatelessWidget {
+  const ColorfullContainerWidget({
+    required this.cardWidth,
+    required this.cardHeight,
+    required this.color,
+    super.key,
+  });
+
+  final double cardWidth;
+  final double cardHeight;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: cardWidth,
+      height: cardHeight,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Icon(Icons.star, color: Colors.yellow),
+              SizedBox(width: 10),
+              Text(
+                'Привіт, Flutter!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 10),
+              Icon(Icons.star, color: Colors.yellow),
+            ],
           ),
         ),
       ),
